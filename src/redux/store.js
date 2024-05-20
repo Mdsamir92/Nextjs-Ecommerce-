394f09cfb110252d1588e6bc6986eb071bf75975
Mohammad Samir <87142605+Mdsamir92@users.noreply.github.com>
2024-05-20 12:25:50 +0530

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,19 +1,3 @@
-// import { configureStore } from '@reduxjs/toolkit' 
-// import { cartSlice } from './cartSlice'
-// // import { createStore } from "redux";
-// import rootred from "../redux/Reducers/main";
-
-
-// export const store = configureStore({
-//   reducer: {
-//     cart : cartSlice,
-//     rootred
-//   },
-// })
-
-
-
-// export default store;
 
 import { createStore } from "redux";
 import rootred from "../redux/Reducers/main";
