e9efa4f79735c16fb17392d245948cae783b5cb2
Mohammad Samir <87142605+Mdsamir92@users.noreply.github.com>
2024-05-20 21:03:14 +0530

    Update page.js

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -41,11 +41,8 @@ function page() {
       try {
       setLoading(true);
       const res = await axios.post('http://localhost:3000/api/users/login', user);
-      console.log("login success", res.data)
-          if(typeof window !== 'undefined'){
-        JSON.stringify(localStorage.setItem('users', user.email));
-      }
-  
+      console.log("login success", res.data)    
+      JSON.stringify(localStorage.setItem('users', user.email));
       toast.success("Login successfully😍")
       router.push('/');
     }
