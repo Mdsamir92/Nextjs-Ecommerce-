3e6458b680703257cf66ddd2c658b801fc80a7a1
Mohammad Samir <87142605+Mdsamir92@users.noreply.github.com>
2024-05-20 15:22:06 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,13 @@
 import React from 'react'
 import Product from './components/allproducts/Product'
 import Navbar from './components/Navbar/Navbar'
-import Footer from "./components/Footer/Footer";
+// import Footer from "./components/Footer/Footer";
 
+import dynamic  from "next/dynamic";
+
+const Footer = dynamic(() => import("./components/Footer/Footer"), {
+  ssr: false,
+});
 function page() {
 
 
