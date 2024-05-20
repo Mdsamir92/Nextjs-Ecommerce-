8f3f0940b59019ae3957a92ab8854aba3febbf36
Mohammad Samir <87142605+Mdsamir92@users.noreply.github.com>
2024-05-20 19:30:23 +0530

    Update dbConn.js

--- src/DbConn/dbConn.js
+++ src/DbConn/dbConn.js
@@ -1,13 +1,13 @@
-// import mongoose from "mongoose";
+import mongoose from "mongoose";
 
 
-// export async function connect() {
-//     try {
+export async function connect() {
+    try {
 
-//         mongoose.connect(process.env.MONGO_URL)
-//         console.log("connect successful..");
+        mongoose.connect(process.env.MONGO_URL)
+        console.log("connect successful..");
         
-//     } catch (error) {
-//         console.log(error, "something went wrong !");
-//     }
-// }
+    } catch (error) {
+        console.log(error, "something went wrong !");
+    }
+}
