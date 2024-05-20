0adc20d8a8b0d214f1bdb46a35dd7a65c76cd017
Mohammad Samir <87142605+Mdsamir92@users.noreply.github.com>
2024-05-20 19:44:40 +0530

    Update userModal.js

--- src/models/userModal.js
+++ src/models/userModal.js
@@ -1,36 +1,36 @@
 
-// import mongoose, { Schema,  } from "mongoose";
+import mongoose, { Schema,  } from "mongoose";
 
-// const userSchema = new Schema(
-//   {
-//     username: {
-//       type: String,
-//       required: true,
-//     },
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
 
-//     email: {
-//       type: String,
-//       required: true,
-//     },
-//     password: {
-//       type: String,
-//       required: true,
-//     },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
    
-//     isVerified: {
-//         type: Boolean,
-//         default: false,
-//     },
-//     verifyToken:String,
-//     verifyTokenExpiry:Date,
-//     forgotPasswordToken:String,
-//     forgotPasswordExpiry:Date,
+    isVerified: {
+        type: Boolean,
+        default: false,
+    },
+    verifyToken:String,
+    verifyTokenExpiry:Date,
+    forgotPasswordToken:String,
+    forgotPasswordExpiry:Date,
    
     
-//   },
-//   { timestamps: true }
-// );
+  },
+  { timestamps: true }
+);
 
-// const User = mongoose.models.users || mongoose.model("users", userSchema);
-// // if already collection name users in database Than its ok otherwise  || its create  new users collection 
-// export default User;
+const User = mongoose.models.users || mongoose.model("users", userSchema);
+// if already collection name users in database Than its ok otherwise  || its create  new users collection 
+export default User;
