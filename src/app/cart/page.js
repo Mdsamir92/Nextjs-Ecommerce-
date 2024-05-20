11a59ed328d1fed863c9181a0c25d81af4fb1c24
Mohammad Samir <87142605+Mdsamir92@users.noreply.github.com>
2024-05-20 12:28:24 +0530

    Update page.js

--- src/app/cart/page.js
+++ src/app/cart/page.js
@@ -54,7 +54,8 @@ const page = (props) => {
     return (
 
         <div className="container mx-auto px-4 max-w-7xl lg:px-0">
-            <div className="mx-auto max-w-2xl py-8 lg:max-w-7xl">
+         <Navbar/>
+       <div className="mx-auto max-w-2xl py-8 lg:max-w-7xl">
                 <h1 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
                     Shopping Cart
                 </h1>
