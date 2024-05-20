d6dac641b65b6d127461a30ef231393af51e0eec
Mohammad Samir <87142605+Mdsamir92@users.noreply.github.com>
2024-05-20 17:46:04 +0530

    Update route.js

--- src/app/api/users/signup/route.js
+++ src/app/api/users/signup/route.js
@@ -1,30 +1,30 @@
-import { connect } from "@/DbConn/dbConn";
-import User from "@/models/userModal";
-import {NextResponse } from 'next/server'
+// import { connect } from "@/DbConn/dbConn";
+// import User from "@/models/userModal";
+// import {NextResponse } from 'next/server'
 
 
-connect();
+// connect();
 
-export async function POST(req,res){
+// export async function POST(req,res){
 
-    try{
+//     try{
  
-    const {username,email,password} = await req.json();
+//     const {username,email,password} = await req.json();
 
-    const usersame = await User.findOne({email})
-    if(usersame){
-     return NextResponse.json({error:" Email already registered"},{status:400})
-    }
+//     const usersame = await User.findOne({email})
+//     if(usersame){
+//      return NextResponse.json({error:" Email already registered"},{status:400})
+//     }
 
-     else{
+//      else{
 
-        const user = new User({username, email, password});
-        await user.save();
-        return NextResponse.json({message:"Register successfully!"},{status:200})
-    }
+//         const user = new User({username, email, password});
+//         await user.save();
+//         return NextResponse.json({message:"Register successfully!"},{status:200})
+//     }
 
-}catch(err){
-    return NextResponse.json({err:"error"},{status:500})
-}
+// }catch(err){
+//     return NextResponse.json({err:"error"},{status:500})
+// }
 
-}
+// }
