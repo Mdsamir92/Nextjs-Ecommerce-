64f0bd179693543b37e329ac7bdbb6dd614ea79e
Mohammad Samir <87142605+Mdsamir92@users.noreply.github.com>
2024-05-20 12:23:03 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,17 @@
 'use client'
-
 import React from 'react'
 import Product from './components/allproducts/Product'
-
-
+import Navbar from './components/Navbar/Navbar'
+import Footer from "./components/Footer/Footer";
 
 function Home() {
 
 
   return (
     <div >
-    
+      <Navbar />
       <Product />
-
+      <Footer/>
     </div>
   )
 }
