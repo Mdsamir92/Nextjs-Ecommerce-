70c6524daec5d5400de6bab65658fedda9aef6ee
Mohammad Samir <87142605+Mdsamir92@users.noreply.github.com>
2024-05-20 19:29:38 +0530

    Update route.js

--- src/app/api/users/login/route.js
+++ src/app/api/users/login/route.js
@@ -1,41 +1,41 @@
-// import { connect } from "@/DbConn/dbConn";
-// import User from "@/models/userModal";
-// import { NextResponse } from 'next/server'
-// import  jwt  from "jsonwebtoken";
+import { connect } from "@/DbConn/dbConn";
+import User from "@/models/userModal";
+import { NextResponse } from 'next/server'
+import  jwt  from "jsonwebtoken";
 
 
-// connect();
+connect();
 
-// export async function POST(req, res) {
+export async function POST(req, res) {
 
-//   try {
+  try {
 
-//     const { email, password } = await req.json();
-//     const usersame = await User.findOne({ email, password })
+    const { email, password } = await req.json();
+    const usersame = await User.findOne({ email, password })
   
-//     if (!usersame) {
-//       return NextResponse.json({ error: " please register than login" }, { status: 400 })
-//     }
+    if (!usersame) {
+      return NextResponse.json({ error: " please register than login" }, { status: 400 })
+    }
 
-//     else {
-//     const tokenData = {
-//     id:usersame._id,
-//     username:usersame.username,
-//     email:usersame.email,
-//     }
-//      const token =    jwt.sign(tokenData,process.env.TOKEN_SECRET,{expiresIn:"1d"})
-//      const response =  NextResponse.json({message: "Login successfully!" }, { status: 200 })
+    else {
+    const tokenData = {
+    id:usersame._id,
+    username:usersame.username,
+    email:usersame.email,
+    }
+     const token =    jwt.sign(tokenData,process.env.TOKEN_SECRET,{expiresIn:"1d"})
+     const response =  NextResponse.json({message: "Login successfully!" }, { status: 200 })
 
-//     response.cookies.set("token",token,{
-//       httpOnly:true,
-//     })
-//     return response;
+    response.cookies.set("token",token,{
+      httpOnly:true,
+    })
+    return response;
 
-//   }
+  }
 
 
-//   } catch (err) {
-//     return NextResponse.json({ err: "error" }, { status: 500 })
-//   }
+  } catch (err) {
+    return NextResponse.json({ err: "error" }, { status: 500 })
+  }
 
-// }
+}
