1f4209b175aa6902e5eba886fe32b2df6a986c0a
Mohammad Samir <87142605+Mdsamir92@users.noreply.github.com>
2024-05-20 17:44:56 +0530

    Update Navbar.js

--- src/app/components/Navbar/Navbar.js
+++ src/app/components/Navbar/Navbar.js
@@ -36,12 +36,11 @@ const Navbar = () => {
 
     // logout function 
     const logout = async () => {
-        const res = await axios.post("http://localhost:3000/api/users/logout",)
-        if (res.data) {
+      
             localStorage.clear('users');
             toast.success("logout successfully!")
             router.push("/login")
-        }
+            
     }
 
     const handleClose = () => {
